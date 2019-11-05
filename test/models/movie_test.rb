3a6445966a7e0a63ac4fc7654dd1c6922d7ee265
KKennedyCodes <holmstedtk@gmail.com>
2019-11-05 13:36:58 -0800

    Movie Tests Passing

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -2,9 +2,22 @@ require "test_helper"
 
 describe Movie do
   describe "validations" do
+    let(:movie) {movies(:movie_one)}
+    
     it "can be created" do
-      movie = movies(:movie_one)
       expect(movie.valid?).must_equal true
     end
+    
+    it "requires title, inventory" do
+      required_fields = [:title, :inventory]
+      
+      required_fields.each do |field|
+        movie[field] = nil
+        
+        expect(movie.valid?).must_equal false
+        
+        movie.reload
+      end
+    end 
   end
 end
