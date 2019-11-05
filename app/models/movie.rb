83bc51d35846553c73253e86f7e1f652c37db740
Eve Le <nguyenthao.lee@gmail.com>
2019-11-05 14:10:32 -0800

    Added validations for Movie model and Customer model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, uniqueness: true, presence: true
+  validates :release_date, presence: true
+  validates :inventory, presence: true, numericality: { greater_than_or_equal_to: 0, message: "must be greater than or equal to 0" }
 end
