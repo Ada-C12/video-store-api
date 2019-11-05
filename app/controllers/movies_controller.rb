f5c4cb9ba0b7212b69ada56e25fb0141708ff156
Morgan Schuler <morganlschuler@gmail.com>
2019-11-05 14:57:38 -0800

    completed migrations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,10 @@
 class MoviesController < ApplicationController
+  
+  def index
+    render json: { ready_for_lunch: "yassss" }, status: :ok
+    
+    # zomg = Movies.all.as_json(only: id:, :registered_at, :address, :city, :state, :postal_code, :phone)
+    # render json: movies, status: ok
+  end
+  
 end
