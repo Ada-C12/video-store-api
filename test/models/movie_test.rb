416adb1755f4aa94ca231034db4cd146108a5be5
Rinostar <codingrinostar@gmail.com>
2019-11-05 14:52:33 -0800

    Update test_helper; Add movies.yml; Finish validation tests for Movie

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 describe Movie do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    before do 
+      @movie = Movie.new(
+        title: "fake movie",
+        overview: "summary",
+        release_date: Date.today,
+        inventory: 1
+      )
+    end
+
+    it "is avlid when all fields are present" do
+      result = @movie.valid?
+
+      expect(result).must_equal true
+    end
+
+    it "is invalid without a title" do
+      @movie.title = nil
+      result = @movie.valid?
+
+      expect(result).must_equal false
+    end
+
+    it "is invalid with a duplicate title" do
+      @movie.title = 'Titanic'
+      result = @movie.valid?
+
+      expect(result).must_equal false
+    end 
+
+    it "is invalid without a release_date" do
+      @movie.release_date= nil
+      result = @movie.valid?
+
+      expect(result).must_equal false
+    end
+
+    it "is invalid without a inventory number" do
+      @movie.inventory= nil
+      result = @movie.valid?
+
+      expect(result).must_equal false
+    end
+
+    it "is invalid without a invalid inventory number" do
+      @movie.inventory= -1
+      result = @movie.valid?
+      
+      expect(result).must_equal false
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,23 @@
+require "simplecov"
+SimpleCov.start "rails" do
+  add_filter "/lib/"
+  add_filter "/vendor/"
+  add_filter "/app/channels"
+  add_filter "/app/jobs"
+  add_filter "/app/mailers"
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/rails"
+require "minitest/reporters"
+
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
