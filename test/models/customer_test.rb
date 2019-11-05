b1ab0d6c7754b25da090bb4123a16c876099cf64
Eve Le <nguyenthao.lee@gmail.com>
2019-11-05 15:30:42 -0800

    Added tests for customer validation

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @valid_customer = customers(:customer1)
+  end
+
+  it "is avlid when all fields are present" do
+    result = @valid_customer.valid?
+
+    expect(result).must_equal true
+  end
+
+  it "is invalid without a name" do
+    @valid_customer.name = nil
+    result = @valid_customer.valid?
+
+    expect(result).must_equal false
+  end
+
+  it "is invalid with a duplicate name" do
+    @valid_customer.name = customers(:customer2).name
+    result = @valid_customer.valid?
+
+    expect(result).must_equal false
+  end 
+
+  it "is invalid without a registered date" do
+    @valid_customer.registered_at = nil
+    result = @valid_customer.valid?
+
+    expect(result).must_equal false
+  end
+
+  it "is invalid without a postal code" do
+    @valid_customer.postal_code = nil
+    result = @valid_customer.valid?
+
+    expect(result).must_equal false
+  end
+
+  it "is invalid without a phone number" do
+    @valid_customer.phone = nil
+    result = @valid_customer.valid?
+    
+    expect(result).must_equal false
+  end
 end
