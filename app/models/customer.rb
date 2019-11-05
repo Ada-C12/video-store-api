8040b474d92008770be54efadbeba6220475bec0
Kristina <kristina.tanya@gmail.com>
2019-11-05 14:27:57 -0800

    remove validation for customer model, registered_at

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   has_many :rentals
   validates :name, presence: true
-  validates :registered_at, presence: true
+  # validates :registered_at, presence: true
   validates :address, presence: true
   validates :city, presence: true
   validates :state, presence: true
