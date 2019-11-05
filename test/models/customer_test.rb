8b1b9b44a0703787b9960bf9fdbc4910d22809fb
Janice Huang <janicehuang@gmail.com>
2019-11-05 15:43:47 -0800

    added again rental relationships. Started on customer relationships tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -2,9 +2,22 @@ require "test_helper"
 
 describe Customer do
   describe "relationships" do
-    it "" do
-
+    it "can add a movie through rentals" do
+      new_customer = Customer.new(name: "ada")
+      new_customer.movies << movies(:movie1)
+      new_customer.movies.first.id.must_equal movies(:movie1).id
     end
 
+    it "can have many rentals" do
+      valid_customer = customers(:janice)
+
+      expect(valid_customer.rentals.count).must_equal 1
+      valid_customer.rentals.each do |rental|
+        rental.must_be_kind_of Rental
+      end
+
+    end
   end
+
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/rails'
+require 'minitest/pride'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
