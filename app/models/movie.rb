4110c9c77de799153101a54970fe70968ab745ea
Kristina <kristina.tanya@gmail.com>
2019-11-05 13:09:51 -0800

    created customers model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many: :rentals
   validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+  validates :release_date, presence: true
   validates :inventory, presence: true
 end
