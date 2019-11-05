52a4f4fa241074c07b70889a87a6b209b986ca5c
Farah Davoodi <farahdavoodi@gmail.com>
2019-11-05 14:17:07 -0800

    add movie relationship tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -26,4 +26,20 @@ describe Movie do
       expect(movie.valid?).must_equal false
     end
   end
+
+  describe "relationships" do
+    let(:movie) {movies(:movie_one)}
+    let(:customer) {customers(:customer_one)}
+    let(:rental) {rentals(:rental_one)}
+
+    it "can have rentals" do
+      expect(movie.rentals.first).must_be_instance_of Rental
+      expect(movie.rentals.first.id).must_equal rental.id
+    end
+
+    it "can have customers through rentals" do
+      expect(movie.customers.first).must_be_instance_of Customer
+      expect(movie.customers.first.name).must_equal customer.name
+    end
+  end
 end
