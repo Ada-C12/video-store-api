ac0e0a0cca7f352e859339c383346d1d687ff301
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-05 14:54:46 -0800

    Added Rental controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   resources :movies, only: [:index, :show, :create]
   
+  post "/rentals/check-out", to: "rentals#create", as: "checkout"
+  patch "/rentals/check-in", to: "rentals#update", as: "checkin"
   get "/customers", to: "customers#index", as: "customers"
 end
