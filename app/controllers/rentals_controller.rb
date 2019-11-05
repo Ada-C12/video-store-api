f682d3fecf9002d3fff01cf2fc22b72fc8f54162
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-11-05 15:06:56 -0800

    Updates to Customer model and Movie seed

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -4,7 +4,9 @@ class RentalsController < ApplicationController
     rental = Rental.new(movie: Movie.first, customer: Customer.first)
     rental.checkout_date = Date.today
     rental.due_date = Date.today + 7
-    if rental.save
+
+    if rental.save && rental.movie.available_inventory > 0 
+      rental.check_out_rental
       render json: rental.as_json(only: [:id]), status: :ok
       return
     else
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,9 @@
 class Rental < ApplicationRecord
   belongs_to :movie
   belongs_to :customer
+
+  def check_out_rental
+    self.movie.available_inventory -= 1
+    self.customer.movies_checked_out_count += 1
+  end
 end
