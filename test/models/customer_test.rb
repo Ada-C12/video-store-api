ccc6990c04cbb39671112d3410a624c5ab11739a
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-05 14:21:21 -0800

    Added Rental model and tests for relationship with Customer & Movie

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  
+  describe "relationships" do
+    it "can have many rentals" do
+      movie = Movie.create(title: "valid movie")
+      customer = Customer.create(name: "valid customer")
+      rental = Rental.create(movie_id: movie.id, customer_id: customer.id)
+      
+      customer.must_respond_to :rentals
+      customer.rentals.each do |rental|
+        rental.must_be_kind_of Rental
+      end
+      expect(customer.rentals.length).must_equal 1
+    end
+  end
+  
 end
---
+++ test/models/rental_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+describe Rental do
+
+  describe "validation" do
+    it 
+  end
+
+end
