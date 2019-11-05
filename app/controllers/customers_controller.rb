5331c125162b67a95679c2abe3547c223007c3d2
Nicky Choi <nickyjinchoi@gmail.com>
2019-11-05 14:12:58 -0800

    Modified index action of customers controller and added tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,11 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all.as_json
+
+    if customers.empty?
+      render json: { message: "No customers" }, status: :ok
+    else
+      render json: customers, status: :ok
+    end
+  end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "responds with JSON and success" do
+      get customers_path
+
+      expect(response.header["Content-Type"]).must_include "json"
+      must_respond_with :ok
+    end
+
+    it "responds with an array of customer hashes" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+
+      names = Customer.columns.map { |column| column.name }
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+        expect(customer.keys.sort).must_equal names.sort
+      end
+    end
+    it "responds with a message and empty array if there are no customers" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      expect(body.keys).must_include "message"
+      assert_equal "No customers", body["message"]
+    end
+  end
 end
