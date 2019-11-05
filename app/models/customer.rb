3b3e9ef8bfd3a78bf25455730c0a1490b6a2d81b
KKennedyCodes <holmstedtk@gmail.com>
2019-11-05 13:12:43 -0800

    Added Validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
   has_many :rentals
   has_many :movies, through: :rentals
+  
+  validates :name, presence: true
+  validates :postal_code, presence: true
+  validates :registered_at, presence: true
 end
