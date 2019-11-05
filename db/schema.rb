b78ac3fa248efbd123af8b56665a4525f0f7d30b
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-11-05 15:07:33 -0800

    FOR REAL schema DONE.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_223141) do
+ActiveRecord::Schema.define(version: 2019_11_05_230604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_223141) do
     t.string "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "registered_at"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -37,4 +38,17 @@ ActiveRecord::Schema.define(version: 2019_11_05_223141) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "rentals", force: :cascade do |t|
+    t.date "due_date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "customer_id"
+    t.bigint "movie_id"
+    t.index ["customer_id"], name: "index_rentals_on_customer_id"
+    t.index ["movie_id"], name: "index_rentals_on_movie_id"
+  end
+
+  add_foreign_key "rentals", "customers"
+  add_foreign_key "rentals", "movies"
 end
