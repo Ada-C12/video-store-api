28f3434459c58af6891cfbcc566683a9ea0cf502
Kristina <kristina.tanya@gmail.com>
2019-11-05 13:59:10 -0800

    added minitest stuff to test_helper

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 describe Movie do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "instantiation" do
+    it "can instantiate a valid product" do
+      new_movie = Movie.new(
+        title: "Titanic",
+        overview: "The door wasn't big enough for two.",
+        release_date: Time.now,
+        inventory: 10,
+      )
+      expect(new_movie.save).must_equal true
+    end
+
+  end
+  
+  describe "relationships" do
+    it "can have many rentals" do
+      # rental_1 = Rental.new
+      # rental_2 = Rental.new
+      # rental_3 = Rental.new
+
+    end
+  end
+
+  describe "validations" do
+    it "must have a title" do
+    end
+
+    it "inventory can't be nil" do
+    end
+  end
+
+
 end
