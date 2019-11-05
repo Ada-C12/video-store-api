ab044816bab8a3fde3b0805173ddca4418e2205d
Natalie Tapias <nataliemtapias@gmail.com>
2019-11-05 14:26:38 -0800

    Movies#show action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,13 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies.as_json(only: [:id, :release_date, :title]), status: :ok
   end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      render json: movie.as_json(only: [:id, :release_date, :title]), status: :ok
+    else
+      render json: {"errors" => {"id" => ["Movie with id #{params[:id]} not found."]}}
+    end
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -4,6 +4,7 @@ describe MoviesController do
   describe 'index' do
     it 'responds with json and success' do
       get movies_path
+      
       expect(response.header['Content-Type']).must_include 'json'
       must_respond_with :ok
     end
@@ -20,6 +21,33 @@ describe MoviesController do
         expect(movie).must_be_instance_of Hash
         expect(movie.keys.sort).must_equal ["id", "release_date", "title"]
       end
+    end
+  end
+
+  describe "show" do
+    it 'responds with json and success' do
+      get movie_path(movies(:first))
+
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+
+    it 'responds with the correct movie hash' do
+      get movie_path(movies(:second))
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Hash
+      expect(body["title"]).must_equal movies(:second).title
+    end
+
+    it 'responds with error when given incorrect parameter' do
+      get movie_path(-1)
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Hash
+      expect(body["errors"]["id"]).must_equal ["Movie with id -1 not found."]
     end
   end
 end
