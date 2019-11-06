6f87cde6bdb8f51c5feec2bbed0933bca3b139f5
Mariya Burrows <mariya.burrows@gmail.com>
2019-11-05 16:32:38 -0800

    added ymls for customer and movies, added controllers for customer and movie, fleshed out the customer tests, all passing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,84 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relationships" do 
+  end
+  
+  describe "validations" do 
+    before do 
+      @mariya = customers(:mariya)
+    end 
+    
+    it "has a name" do 
+      # Assert
+      @mariya.name.must_equal "mariya"
+    end
+    
+    it "will fail if it doesnt have a name" do
+      bad_user = Customer.new(name: nil, address: "801 Dexter Ave", registered_at: Date.parse("Wed, 29 Apr 2015"), city: "Seattle", state: "WA", postal_code: "98116")
+      
+      expect(bad_user.valid?).must_equal false
+    end
+    
+    it "has registered_at" do 
+      # Assert
+      @mariya.registered_at.wont_be_nil
+    end
+    
+    it "will fail if it isnt registered" do
+      bad_user = Customer.new(name: "Cloudy", address: "801 Dexter Ave", registered_at: nil, city: "Seattle", state: "WA", postal_code: "98116")
+      
+      expect(bad_user.valid?).must_equal false
+    end
+    
+    it "has an address" do 
+      @mariya.address.wont_be_nil
+    end
+    
+    it "will fail if an address is not provided" do 
+      bad_user = Customer.new(name: "Cloudy", address: nil, registered_at: Date.parse("Wed, 29 Apr 2015"), city: "Seattle", state: "WA", postal_code: "98116")
+      
+      expect(bad_user.valid?).must_equal false
+    end
+    
+    it "has a city" do 
+      @mariya.city.wont_be_nil
+    end
+    
+    it "will fail if a city is not provided" do 
+      bad_user = Customer.new(name: "Cloudy", address: "801 Dexter Ave", registered_at: Date.parse("Wed, 29 Apr 2015"), city: nil, state: "WA", postal_code: "98116")
+      
+      expect(bad_user.valid?).must_equal false
+    end
+    
+    it "has a state" do 
+      @mariya.state.wont_be_nil
+    end
+    
+    it "will fail if a state is not provided" do
+      bad_user = Customer.new(name: "Cloudy", address: "801 Dexter Ave", registered_at: Date.parse("Wed, 29 Apr 2015"), city: "Seattle", state: nil, postal_code: "98116")
+      
+      expect(bad_user.valid?).must_equal false
+    end
+    
+    it "has a postal code" do 
+      @mariya.postal_code.wont_be_nil
+    end
+    
+    it "will fail if a zipcode is not provided" do
+      bad_user = Customer.new(name: "Cloudy", address: "801 Dexter Ave", registered_at: Date.parse("Wed, 29 Apr 2015"), city: "Seattle", state: "WA", postal_code: nil)
+      
+      expect(bad_user.valid?).must_equal false
+    end
+    
+    it "has a phone number" do 
+      @mariya.phone.wont_be_nil
+    end
+    
+    it "will fail if a phone number is not listed" do
+      bad_user = Customer.new(name: "Cloudy", address: "801 Dexter Ave", registered_at: Date.parse("Wed, 29 Apr 2015"), city: nil, state: "WA", postal_code: "98116")
+      
+      expect(bad_user.valid?).must_equal false
+    end
+  end
 end
