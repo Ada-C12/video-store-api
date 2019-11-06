d6d8959ef2fee0e94ec228ad711ca25fb926b784
KKennedyCodes <holmstedtk@gmail.com>
2019-11-05 16:09:19 -0800

    Added Movie Controller

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,2 +1,28 @@
+KEYS = [:id, :title, :overview, :release_date, :inventory]
+
 class MovieController < ApplicationController
+  def index
+    movies = Movie.all.as_json(only: KEYS)
+    render json: movies, status: :ok
+  end
+  
+  def show
+    movie_id = params[:id]
+    movie = Movie.find_by(id: movie_id)
+    
+    if movie
+      render json: movie.as_json(only: KEYS), status: :ok
+      return
+    else
+      render json: {"errors"=>["not found"]}, status: :not_found
+      return
+    end
+  end
+  
+  private
+  
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
+  end
 end
+
