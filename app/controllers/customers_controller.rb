654f06942ac30495eba57e4b578441ce387af355
Alice <alicesunhi@gmail.com>
2019-11-06 14:54:41 -0800

    customer json field

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,10 @@
 class CustomersController < ApplicationController
   
   def index
-    #needs movies_checked_out_count
     customers = Customer.all.as_json(only: [:id, :name, :registered_at, :postal_code, :phone])
     render json: customers, status: :ok
   end
+  
   
 end
 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,16 @@
 class Customer < ApplicationRecord
   has_many :rentals, dependent: :destroy
+  
+  def movies_checked_out_count
+    return self.rentals.count
+  end
+  
+  
+  #https://blog.arkency.com/how-to-overwrite-to-json-as-json-in-active-record-models-in-rails/
+  def as_json(*)
+    super.except("created_at", "updated_at").tap do |hash|
+      hash["movies_checked_out_count"] = movies_checked_out_count
+    end
+  end
+  
 end
