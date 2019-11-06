9100bc03246545ff9137222c2dc4d385df9f9fe4
Hallie <idhallie@gmail.com>
2019-11-06 15:10:26 -0800

    fixed model tests, added customer index tests.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  KEYS = [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at3, :registered_at, :created_at]
+  KEYS = [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at]
   
   def index
     customers = Customer.all.as_json(only: KEYS)
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "responds with JSON and success" do
+      get customers_path
+      
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :success
+    end
+    
+    it "responds with an array of customer hashes" do
+      # Act
+      get customers_path
+      
+      # Get the body of the response
+      body = JSON.parse(response.body)
+      
+      # Assert
+      expect(body).must_be_instance_of Array
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+        expect(customer.keys.sort).must_equal %w[id name address city state postal_code phone registered_at].sort
+      end
+    end
+    
+    it "will respond with an empty array when there are no customers" do
+      # Arrange
+      Customer.destroy_all
+      
+      # Act
+      get customers_path
+      body = JSON.parse(response.body)
+      
+      # Assert
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
+  end
 end
