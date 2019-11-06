36720d5c7bf5da8a72bfe01e1eb84eebdd19eaaf
Kristina <kristina.tanya@gmail.com>
2019-11-06 12:40:23 -0800

    passing test to create one rental

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,3 +1,24 @@
 class RentalsController < ApplicationController
-  
+  def checkout
+    rental = Rental.new(rental_params)
+    rental.checkout_date = Time.now
+    rental.due_date = Time.now + 7
+
+    if rental.save
+      render json: rental.as_json(only: [:id]), status: :created
+      return
+    else
+      render json: {
+        ok: false,
+        errors: rental.errors.messages
+        }, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:movie_id, :customer_id)
+  end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -9,5 +9,45 @@ describe RentalsController do
 
     # movie_id is passed in the request body
     # customer_id passed in the request body
+
+    # once the rental is created, reduce the inventory of that movie 
+    # by one. -- maybe a model method?
+
+    it "will create a new instance of a rental" do
+      # rental = Rental.new(
+      #   checkout_date: Time.now,
+      #   due_date: Time.now + 7,
+      #   movie: movies(:movie1),
+      #   customer: customers(:customer1),
+      # )
+      movie = movies(:movie1)
+      customer = customers(:customer1)
+      rental_data = {
+        rental: {
+          movie_id: movie.id,
+          customer_id: customer.id,
+        }
+      }
+
+      expect {
+        post rental_checkout_path, params: rental_data
+      }.must_differ "Rental.count", 1
+      
+      must_respond_with :created
+    end
+
+    it "will have the correct attributes" do
+    end
+
+    it "will not create a rental with missing attributes" do
+    end
+
+    it "will reduce the inventory of a movie by 1" do
+    end
+
+    it "will not rent a movie that is out of stock" do
+    end
+
+
   end
 end
