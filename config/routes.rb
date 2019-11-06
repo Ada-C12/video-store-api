18a8da6bfa7254e5741a7200adecd3de07a6c41f
Farah Davoodi <farahdavoodi@gmail.com>
2019-11-06 14:33:59 -0800

    added new routes for check in check out

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :movies
   resources :customers
   resources :rentals
+  post "/rentals/check-out", to: "rentals#checkout", as: "checkout"
+  post "rentals/check-in", to: "rentals#checkin", as: "checkin"
 end
