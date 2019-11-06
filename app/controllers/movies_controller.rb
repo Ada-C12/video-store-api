61691cee72d34fc342977bc594adf3391abd2d04
eaball35 <eaball35@gmail.com>
2019-11-06 13:23:27 -0800

    post movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+
+  def create
+    new_movie = Movie.new(movie_params)
+    if new_movie.save
+      render json: new_movie.as_json(only: [:id]), status: :created
+    else
+      render json: {errors: ["errors"]}, status: :bad_request
+    end
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,5 +1,27 @@
 require "test_helper"
 
 describe MoviesController do
-  
-end
+  before do
+    @movie = {
+      movie: {
+        field: "field",
+      }
+    }
+  end
+  it 'responds with created status when request is good' do
+    expect(post movies_path, params: @movie).must differ "Movie.count", 1
+    must_respond_with :created
+    body = JSON.parse(response.body)
+    expect(body.keys).must_equal ['id']
+  end
+
+  it 'responds with bad_request when request has no field' do
+    @movie[:movie][:field] = nil
+    
+    expect(post movies_path, params: @movie).wont_change 'Movie.count'
+    must_respond_with :bad_request
+    
+    body = JSON.parse(response.body)
+    expect(body['errors'].must_include 'field')
+  end
+end 
