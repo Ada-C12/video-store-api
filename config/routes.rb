b0c6ef8e4ecef7548799d685ca4c28658720f14e
Natalie Tapias <nataliemtapias@gmail.com>
2019-11-06 12:43:18 -0800

    routes for movies#checkin and movies#checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :customers, only: [:index]
   resources :movies, only: [:index, :show, :create]
+
+  post "/rentals/check-out", to: "movies#checkout", as: "checkout_movie"
+  post "/rentals/check-in", to: "movies#checkin", as: "checkin_movie"
 end
