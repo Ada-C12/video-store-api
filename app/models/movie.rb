b792bea510c2100883f311061e27edfe4636266e
KKennedyCodes <holmstedtk@gmail.com>
2019-11-06 15:13:08 -0800

    Added Movie Model Method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,14 @@ class Movie < ApplicationRecord
   
   validates :title, presence: true
   validates :inventory, presence: true
+  
+  def checkout(movie)
+    Checkout_Movie = Movie.find_by(movie.id)
+    if Checkout_Movie.available_inventory > 1
+      Checkout_Movie.available_inventory -= 1 
+    else
+      raise error
+      render json: {"errors"=>["no inventory available"]}, status: :error
+    end
+  end
 end
