e7b83a84f0e28bb6996dbc853d1ad465333f5557
Sam <samcoll@Samanthas-MBP.localdomain>
2019-11-06 15:57:12 -0800

    updated schema and fixtures

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@ class CustomersController < ApplicationController
   
   def index 
     @customers = Customer.all
-    customers = Customer.all.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone])
+    customers = Customer.all.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count])
     render json: customers, status: :ok 
   end 
   
@@ -10,7 +10,7 @@ class CustomersController < ApplicationController
     customer = Customer.find_by(id: params[:id])
     
     if customer
-      render json: customer.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone])
+      render json: customer.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count])
     else 
       not_found
     end 
@@ -30,7 +30,7 @@ class CustomersController < ApplicationController
   private 
   
   def customer_params
-    params.require(:customer).permit(:name, :registered_at, :address, :city, :state, :postal_code, :phone)
+    params.permit(:name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count)
   end 
   
 end
