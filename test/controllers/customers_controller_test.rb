d9c3db692ec84193919a6ddc33acbef0231c2c4c
Mariya Burrows <mariya.burrows@gmail.com>
2019-11-06 14:04:42 -0800

    added customer controller tests

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do 
+    it "responds with JSON and success" do 
+      # Act
+      get customers_path
+
+      # Assert
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+
+    CUSTOMER_FIELDS = ["address", "city", "id", "movies_checked_out_count", "name", "phone", "postal_code", "registered_at", "state"]
+
+    it "responds with an array of customer hashes" do 
+      # Act
+      get customers_path 
+      body = JSON.parse(response.body)
+
+      # Assert
+      expect(body).must_be_instance_of Array 
+      expect(body[0]).must_be_instance_of Hash 
+    end
+
+    it "will respond with an empty array if there are no customers" do 
+      # Arrange
+      Customer.destroy_all
+
+      # Act
+      get customers_path 
+      body = JSON.parse(response.body)
+
+      # Assert
+      expect(body).must_be_instance_of Array 
+      expect(body).must_equal []
+
+    end
+  end
 end
