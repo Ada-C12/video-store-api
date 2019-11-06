991c8ba534ead95683f0e643aa1012de2619d628
brilatimer <brihiramine@gmail.com>
2019-11-06 13:18:07 -0800

    created rental yml, rental validation and relations tests written, some passing

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,65 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:rental) {rentals(:r1)}
+  
+  describe "validations" do
+    
+    it "is valid with all fields present and valid" do
+      expect(rental.valid?).must_equal true
+    end
+    
+    it "is invalid without a customer_id" do
+      rental.customer_id = nil
+      
+      expect(rental.valid?).must_equal false
+      expect(rental.errors.messages).must_include :customer_id
+    end
+    
+    it 'is invalid without a customer_id' do
+      rental.customer_id = nil
+      
+      expect(rental.valid?).must_equal false
+      expect(rental.errors.messages).must_include :customer_id
+    end
+    
+    it 'is invalid without movie_id' do
+      rental.movie_id = nil
+      
+      expect(rental.valid?).must_equal false
+      expect(rental.errors.messages).must_include :movie_id
+    end
+    
+    
+    it 'is invalid when movie_id is not the right datatype' do
+      rental.movie_id = nil
+      
+      expect(rental.valid?).must_equal false
+      expect(rental.errors.messages).must_include :movie_id
+    end
+    
+    it 'is invalid without a created_at' do
+      rental.created_at = nil
+      
+      expect(rental.valid?).must_equal false
+      expect(rental.errors.messages).must_include :created_at
+    end
+    
+    it 'is invalid without a updated_at' do
+      rental.updated_at = nil
+      
+      expect(rental.valid?).must_equal false
+      expect(rental.errors.messages).must_include :updated_at
+    end    
+  end
+  
+  describe "relations" do
+    it "can have one or many rentals" do
+      rental.must_respond_to :rentals
+      rental.rentals.each do |rental|
+        rental.must_be_kind_of Rental
+      end
+    end    
+  end
 end
+
