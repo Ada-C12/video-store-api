1cef1e8d34eafd4799de321cd28c615f791a96ff
Janice Huang <janicehuang@gmail.com>
2019-11-05 20:56:13 -0800

    add GET customers controller test

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,10 @@
 class CustomersController < ApplicationController
-  
+
   def zomg
     working = "it works!"
     render json: working, status: :ok
   end
+
   
+
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "responds with an array of customer hashes" do
+    get customers_path
+    body = JSON.parse(response.body)
+    expect(body).must_be_instance_of Array
+    body.each do |customer|
+      expect(customer).must_be_instance_of Hash
+      expect(customer.keys.sort).must_equal ["address", "city", "movies_checked_out_count", "name", "phone", "postal_code", "state"]
+    end
+  end
 end
