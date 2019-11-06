32c9ad5853216c716a3924d48824718ff74d9530
Brianna Kemp <kemp.bri@gmail.com>
2019-11-05 16:47:15 -0800

    Added movies index and show controller methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,17 @@
 class MoviesController < ApplicationController
+  
+  def index
+    movies = Movie.all.as_json(only: [:id, :title, :release_date])
+    render json: movies, status: :ok
+  end
+  
+  def show
+    movie = Movie.find_by(id: params[:id])
+    
+    if movie
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory])
+    else 
+      render json: {"errors" => ["not found"]}, status: :not_found
+    end
+  end
 end
