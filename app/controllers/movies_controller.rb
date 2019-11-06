95cd8e00fdee4a9dab78dad1a0fe800a8be6bfd6
Ga-Young Jin <gyjinn@gmail.com>
2019-11-05 16:41:35 -0800

    added movie show method and tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,15 @@ class MoviesController < ApplicationController
     
     render :json => movies.as_json(only: [:id, :title, :release_date]), status: :ok
   end
+  
+  def show
+    movie = Movie.find_by_id(params[:id])
+    
+    if movie.nil?
+      render json: {"errors" => {"id" => ["no movie by ID: #{params[:id]}"]}}, status: :not_found
+      return
+    end
+    
+    render :json => movie.as_json(only: [:id, :title, :overview, :release_date, :inventory]), status: :ok
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -32,4 +32,29 @@ describe MoviesController do
       expect(body).must_equal []
     end
   end
+  
+  describe "show" do
+    it "retrieves one movie" do
+      movie = Movie.first
+      
+      get movie_path(movie)
+      body = JSON.parse(response.body)
+      
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal ["id", "inventory", "overview", "release_date", "title"]
+    end
+    
+    it "sends back not found if the movie does not exist" do
+      get movie_path(-1)
+      body = JSON.parse(response.body)
+      
+      must_respond_with :not_found
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]["id"]).must_equal ["no movie by ID: -1"]
+    end
+  end
 end
