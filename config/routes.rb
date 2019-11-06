6c6c672a0b42631cb800613d4e001a5468d84af0
Rinostar <codingrinostar@gmail.com>
2019-11-06 15:05:58 -0800

    create rental model and update routes & database

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :customers, only: [:index]
   resources :movies, only: [:index, :show, :create]
+  
+  post '/rentals/check-out', to: 'rentals#create', as: "checkout"
+  post '/rentals/check-in', to: 'rentals#update', as: "checkin"
 end
