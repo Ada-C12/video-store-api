32c3e034349a207454a7955a343e0c46e4f06175
Monick <monick.keo@gmail.com>
2019-11-06 14:37:14 -0800

    Added index action and tests

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
-# describe CustomersController do
-#   # it "does a thing" do
-#   #   value(1+1).must_equal 2
-#   # end
-# end
+describe CustomersController do
+  CUSTOMER_FIELDS = ['id', 'name', 'phone', 'postal_code', 'registered_at']
+
+  describe "index" do 
+    it "responds with JSON and success" do
+      get customers_path
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+
+    it "will give a list of all customers" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+  
+      expect(body).must_be_instance_of Array
+      expect(body.size).must_equal Customer.count
+
+      body.each do |pet_hash| 
+        expect(pet_hash).must_be_instance_of Hash
+        expect(pet_hash.keys.sort).must_equal CUSTOMER_FIELDS
+      end 
+    end
+  end 
+ 
+end
