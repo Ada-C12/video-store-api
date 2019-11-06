849252b93530fdc1e99089717d626526ebff324c
Mariya Burrows <mariya.burrows@gmail.com>
2019-11-06 13:40:08 -0800

    added column to customers schema

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@ class CustomersController < ApplicationController
 
   def index
     customers = Customer.all 
-    render json: { customers.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone]), customers.movies.count }, status: :ok
+    render json: customers, status: :ok
   end
 
 end
