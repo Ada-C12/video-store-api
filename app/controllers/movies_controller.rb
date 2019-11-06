ccd8fea5cd66a52db12d7cf960c20fe9d52c7dfa
Janice Huang <janicehuang@gmail.com>
2019-11-06 13:45:17 -0800

    add movie show controller tests and method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,15 @@ class MoviesController < ApplicationController
     render json: movies.as_json(only: KEYS), status: :ok
   end
   
+  def show
+    movie_id = params[:id]
+    movie = Movie.find_by(id: movie_id)
+    if movie
+      render json: movie.as_json(only: KEYS)
+      return
+    else
+      render json: {"errors"=>["not found"]}, status: :not_found
+      return
+    end
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+MOVIE_KEYS = ["id", "release_date", "title"]
 
 describe MoviesController do
   describe "index" do
@@ -8,10 +9,10 @@ describe MoviesController do
       expect(body).must_be_instance_of Array
       body.each do |movie|
         expect(movie).must_be_instance_of Hash
-        expect(movie.keys.sort).must_equal ["id", "release_date", "title"]
+        expect(movie.keys.sort).must_equal MOVIE_KEYS
       end
     end
-
+    
     it "will respond with an empty array when there are no movies" do
       Movie.destroy_all
       get movies_path
@@ -20,10 +21,14 @@ describe MoviesController do
       expect(body).must_equal []
     end
   end
-
+  
   describe "show" do
     it "responds with one movie's hash" do
-
+      movie = movies(:movie2)
+      get movie_path(movie.id)
+      body = check_response(expected_type: Hash)
+      expect(body.keys.sort).must_equal MOVIE_KEYS
+      expect(body["title"]).must_equal movie.title
     end
   end
 end
