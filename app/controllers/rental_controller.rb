b67fe9698e2c1240b5424544ede14495ba029534
KKennedyCodes <holmstedtk@gmail.com>
2019-11-05 16:12:12 -0800

    Added Rental Controller

--- app/controllers/rental_controller.rb
+++ app/controllers/rental_controller.rb
@@ -1,2 +1,27 @@
+KEYS = [:id, :checkout_date, :due_date, :rental_id, :customer_id]
+
 class RentalController < ApplicationController
+  def index
+    rentals = Rental.all.as_json(only: KEYS)
+    render json: rentals, status: :ok
+  end
+  
+  def show
+    rental_id = params[:id]
+    rental = Rental.find_by(id: rental_id)
+    
+    if rental
+      render json: rental.as_json(only: KEYS), status: :ok
+      return
+    else
+      render json: {"errors"=>["not found"]}, status: :not_found
+      return
+    end
+  end
+  
+  private
+  
+  def rental_params
+    params.require(:rental).permit(:checkout_date, :due_date, :rental_id, :customer_id)
+  end
 end
