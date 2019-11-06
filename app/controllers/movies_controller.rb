0d4ace1a6ccdb829c9a29d2264a68abba58f51dd
Nicky Choi <nickyjinchoi@gmail.com>
2019-11-05 17:14:03 -0800

    Created controller actions for Movies and added tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,41 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all.as_json(only: [:id, :title, :release_date])
+    render json: movies, status: :ok
+  end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+
+    if movie
+      render json: movie.as_json(only: [:title, :overview, :release_date, :inventory])
+      return
+    else
+      render json: {
+               "errors": ["Not found"],
+             },
+             status: :not_found
+    end
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: movie.as_json(only: [:id]), status: :created
+      return
+    else
+      render json: {
+               ok: false,
+               errors: movie.errors.messages,
+             }, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
+  end
 end
