8418d8bab29f5abc59f39d46e84f31a06cae6ff5
Monick <monick.keo@gmail.com>
2019-11-06 14:41:38 -0800

    Added test to index for returning nothing when there are no customers

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -23,6 +23,15 @@ describe CustomersController do
         expect(pet_hash.keys.sort).must_equal CUSTOMER_FIELDS
       end 
     end
+
+    it "will respond with an empty array when there are no customers" do
+      Customer.destroy_all
+  
+      get customers_path
+      body = JSON.parse(response.body)
+  
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
   end 
- 
 end
