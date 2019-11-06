2b56695906b63c9bb690ae04eeac8d7d26142142
Monick <monick.keo@gmail.com>
2019-11-05 21:17:32 -0800

    wrote all tests for validations. A few not passing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   has_many :rentals
+
+  validates :title, presence: true
+  validates :release_date, presence: true, format: {with: /\d{4}\W\d{2}\W\d{2}/}
+  validates :inventory, presence: true, numericality: { only_integer: true }
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -2,15 +2,81 @@ require "test_helper"
 
 describe Movie do
   before do 
-    @movie = movies(:m1)
+    @valid_movie = movies(:m1)
+    @missing_title_movie = movies(:m2)
+    @missing_release_date_movie = movies(:m3)
+    @invalid_release_date_movie = movies(:m4)
+    @missing_inventory_movie = movies(:m5)
+    @invalid_inventory_movie = movies(:m6)
   end
   
   describe "relations" do
     it "can have one or many rentals" do
-      @movie.must_respond_to :rentals
-      @movie.rentals.each do |rental|
+      @valid_movie.must_respond_to :rentals
+      @valid_movie.rentals.each do |rental|
         rental.must_be_kind_of Rental
       end
     end    
   end
+
+  describe "validations" do 
+    describe "title" do 
+      it "should validiate movie with title (presence)" do 
+        result = @valid_movie.valid?
+        expect(result).must_equal true
+      end 
+  
+      it "should not validate movie without title (presence)" do 
+        result = @missing_title_movie.valid?
+        expect(result).must_equal false
+      end 
+    end 
+    
+    describe "release_date" do 
+      it "should validiate movie with release_date (presence)" do 
+        result = @valid_movie.valid?
+        expect(result).must_equal true
+      end 
+  
+      it "should not validate movie without release_date (presence)" do 
+        result = @missing_release_date_movie.valid?
+        expect(result).must_equal false
+      end 
+  
+      it "should validate correct release_date format" do 
+        #HOW DO WE KNOW THIS IS CHECKING RELEASE DATE FORMAT
+        result = @valid_movie.valid?
+        expect(result).must_equal true
+        # expect(@valid_movie.release_date).must_equal 
+        # maybe /regex/.match(release_date)
+      end
+  
+      it "should not validate incorrect release_date format" do 
+        result = @invalid_release_date_movie.valid?
+        expect(result).must_equal false
+      end 
+    end 
+    
+    describe "inventory" do 
+      it "should validiate movie with inventory (presence)" do 
+        result = @valid_movie.valid?
+        expect(result).must_equal true
+      end 
+  
+      it "should not validate movie without inventory (presence)" do 
+        result = @missing_inventory_movie.valid?
+        expect(result).must_equal false
+      end 
+  
+      it "should validate inventory with integer" do 
+        result = @valid_movie.valid?
+        expect(result).must_equal true
+      end 
+  
+      it "should not validate inventory not with integer" do 
+        result = @invalid_inventory_movie.valid?
+        expect(result).must_equal false
+      end 
+    end 
+  end 
 end
