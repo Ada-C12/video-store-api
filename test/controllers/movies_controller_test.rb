82ab985cc1cc52a13f3bf6da124012636b9b1a46
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-11-06 14:19:09 -0800

    added route for movie index and tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -5,21 +5,33 @@ describe MoviesController do
   #   value(1+1).must_equal 2
   # end
 
-  describe "index" do 
-    it "responds with an array of pet hashes" do
-      # Act
+  describe "index" do
+    it "responds with JSON and success" do
       get movies_path
-  
-      # Get the body of the response
+
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+    it "responds with an array of movie hashes" do
+      get movies_path
+
       body = JSON.parse(response.body)
-  
-      # Assert
+
       expect(body).must_be_instance_of Array
       body.each do |movie|
         expect(movie).must_be_instance_of Hash
-        expect(pet.keys.sort).must_equal 
+        expect(movie.keys.sort).must_equal ["id", "release_date", "title"]
       end
+    end
 
-  end 
+    it "will respond with an empty array when there are no movies" do
+      Movie.destroy_all
 
+      get movies_path
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
+  end
 end
