0db050ee65d10ccc1a618112f41583e6eedab2df
Alice <alicesunhi@gmail.com>
2019-11-05 16:30:35 -0800

    movies controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+    render json: movies.as_json(only: [:id, :title, :release_date]), status: :ok
+  end
+  
+  def show
+    movie = Movie.find_by(id: params[:id])
+    render json: movie.as_json(only: [:title, :overview, :release_date, :inventory])
+  end
+  
+  private
+  
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
+  end
+  
+end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,11 +1,18 @@
 require "test_helper"
 
 describe Customer do
+  let (:customer) {customers(:shelley)}
+  
+  describe "validations" do
+    it "can be created" do
+      expect(customer.valid?).must_equal true
+    end
+  end
+  
   describe "relations" do
     it "has a list of rentals" do
-      shelley = customers(:shelley)
-      _(shelley).must_respond_to :rentals
-      shelley.rentals.each do |rental|
+      _(customer).must_respond_to :rentals
+      customer.rentals.each do |rental|
         _(rental).must_be_kind_of Rental
       end
     end
