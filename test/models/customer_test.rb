61d11afa004c43bf474c689431ee3939c754ed45
brilatimer <brihiramine@gmail.com>
2019-11-05 20:50:09 -0800

    all tests passing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -17,105 +17,63 @@ describe Customer do
     end
     
     it 'is invalid without a name' do
-      invalid_customer = Customer.create(name: "wolfy")
+      customer.name = nil
       
-      expect(invalid_customer.valid?).must_equal false
-      expect(invalid_customer.errors.messages).must_include :name
+      expect(customer.valid?).must_equal false
+      expect(customer.errors.messages).must_include :name
     end
     
-    it 'is invalid without an complete address' do
+    it 'is invalid without address' do
       customer.address = nil
       
       expect(customer.valid?).must_equal false
       expect(customer.errors.messages).must_include :address
     end
     
-    it 'is invalid without an address' do
-      invalid_customer = customer.create(name: "new customer", address: "12456 Seawall Lane")
-      
-      expect(invalid_customer.valid?).must_equal false
-      expect(invalid_customer.errors.messages).must_include :address
-    end
     
-    
-    it 'is invalid when registered_at is not complete' do
+    it 'is invalid when registered_at timestamp' do
       customer.registered_at = nil
       
       expect(customer.valid?).must_equal false
       expect(customer.errors.messages).must_include :registered_at
     end
     
-    it 'is invalid without an registered_at timestamp' do
-      invalid_customer = customer.create(name: "new customer", registered_at: "Wed, 05 Nov 2019 07:54:15 -0700")
-      
-      expect(invalid_customer.valid?).must_equal false
-      expect(invalid_customer.errors.messages).must_include :registered_at
-    end
-    
     it 'is invalid without a city' do
-      customer.registered_at = nil
+      customer.city = nil
       
       expect(customer.valid?).must_equal false
       expect(customer.errors.messages).must_include :city
     end
     
-    it 'is invalid without a complete city' do
-      invalid_customer = customer.create(name: "new customer", city: "Cleaveland")
+    it 'is invalid without a state' do
+      customer.state = nil
       
-      expect(invalid_customer.valid?).must_equal false
-      expect(invalid_customer.errors.messages).must_include :city
-      
-      
-      it 'is invalid without a state' do
-        customer.registered_at = nil
-        
-        expect(customer.valid?).must_equal false
-        expect(customer.errors.messages).must_include :state
-      end
-      
-      it 'is invalid without a complete state' do
-        invalid_customer = customer.create(name: "new customer", state: "Ohio")
-        
-        expect(invalid_customer.valid?).must_equal false
-        expect(invalid_customer.errors.messages).must_include :state
-      end
-      
-      it 'is invalid without a postal_code' do
-        customer.registered_at = nil
-        
-        expect(customer.valid?).must_equal false
-        expect(customer.errors.messages).must_include :postal_code
-      end
-      
-      it 'is invalid without a complete postal_code' do
-        invalid_customer = customer.create(name: "new customer", postal_code: "91208")
-        
-        expect(invalid_customer.valid?).must_equal false
-        expect(invalid_customer.errors.messages).must_include :postal_code
-      end
-      
-      it 'is invalid without a phone' do
-        customer.registered_at = nil
-        
-        expect(customer.valid?).must_equal false
-        expect(customer.errors.messages).must_include :phone
-      end
-      
-      it 'is invalid without a complete phone' do
-        invalid_customer = customer.create(name: "new customer", phone: "719-322-2819")
-        
-        expect(invalid_customer.valid?).must_equal false
-        expect(invalid_customer.errors.messages).must_include :phone
-      end
+      expect(customer.valid?).must_equal false
+      expect(customer.errors.messages).must_include :state
     end
     
-    describe "relations" do
-      it "can have one or many rentals" do
-        @customer.must_respond_to :rentals
-        @customer.rentals.each do |rental|
-          rental.must_be_kind_of rental
-        end
-      end    
+    it 'is invalid without a postal_code' do
+      customer.postal_code = nil
+      
+      expect(customer.valid?).must_equal false
+      expect(customer.errors.messages).must_include :postal_code
     end
+    
+    it 'is invalid without a phone' do
+      customer.phone = nil
+      
+      expect(customer.valid?).must_equal false
+      expect(customer.errors.messages).must_include :phone
+    end
+    
+  end
+  
+  describe "relations" do
+    it "can have one or many rentals" do
+      customer.must_respond_to :rentals
+      customer.rentals.each do |rental|
+        rental.must_be_kind_of rental
+      end
+    end    
   end
 end
