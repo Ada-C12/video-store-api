0be206c6c27a6a364b1c336a04f33ec63c966f54
KKennedyCodes <holmstedtk@gmail.com>
2019-11-05 16:50:32 -0800

    Breaking from Day 1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,6 @@ class Customer < ApplicationRecord
   validates :name, presence: true
   validates :postal_code, presence: true
   validates :registered_at, presence: true
+  validates :phone, presence: true
+  validate :movies_checked_out_count, presence: true
 end
