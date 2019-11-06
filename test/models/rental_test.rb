4bf26c53fdcded9ee61e46853665d9fcef4bf364
dora1405 <li.dora05@gmail.com>
2019-11-05 19:29:43 -0800

    rental model relationships tests

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:rental1) {rentals(:rental1) }
+  describe "relationships" do
+    
+    it "belongs to movie" do
+      expect(rental1.movie).must_be_instance_of Movie
+      expect(rental1.movie).wont_be_nil
+    end
+    
+    it "belongs to customer" do
+      expect(rental1.customer).must_be_instance_of Customer
+      expect(rental1.customer).wont_be_nil      
+    end
+    
+  end
 end
