a19d4738dafec9a92cee26f90aecf04ac507aa9b
Morgan Schuler <morganlschuler@gmail.com>
2019-11-06 14:55:49 -0800

    restored movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,12 +9,13 @@ class MoviesController < ApplicationController
     movie = Movie.find_by(id: params[:id])
     
     if movie
-      render json: movie.as_json(only: :id, :title, :overview, :release_date, :inventory)
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory])
       return
     else
       render json: { ok: false, errors: 'Not found'}, status: :not_found
       return
     end
   end
+  
   
 end
