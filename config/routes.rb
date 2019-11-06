a5baf5aa4700b327a8227e1291ab3d87f417f782
Kristina <kristina.tanya@gmail.com>
2019-11-06 12:16:17 -0800

    add custom route for POST rentals/checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :customers, only: [:index, :show]
   resources :movies, only: [:index, :show, :create]
+  post '/rentals/checkout', to: 'rentals#checkout', as: 'rental_checkout'
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "checkout" do
+    # when POST /rentals/check-out controller action is hit
+    # create a new instance of a rental
+    # checkout_date is set to Time.now
+    # due_date is set to 7 days from checkout_date
+
+    # movie_id is passed in the request body
+    # customer_id passed in the request body
+  end
 end
