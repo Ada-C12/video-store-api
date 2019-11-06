9b5d7819426d751ba8e46ae40700437a0833ca75
Hallie <idhallie@gmail.com>
2019-11-05 16:30:07 -0800

    Added customers route and index.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,8 @@
 class CustomersController < ApplicationController
+  KEYS = [:id, :name, :address, :city, :state, :postal_code, :phone]
+  
+  def index
+    customers = Customer.all.as_json(only: KEYS)
+    render json: customers, status: :ok
+  end
 end
