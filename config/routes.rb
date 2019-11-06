9b1ff234a03a3cf7ed4880e2f3c0e94d311a4c63
Natalie Tapias <nataliemtapias@gmail.com>
2019-11-06 12:44:30 -0800

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :customers, only: [:index]
   resources :movies, only: [:index, :show, :create]
 
-  post "/rentals/check-out", to: "movies#checkout", as: "checkout_movie"
-  post "/rentals/check-in", to: "movies#checkin", as: "checkin_movie"
+  post "/rentals/check-out", to: "rentals#checkout", as: "checkout"
+  post "/rentals/check-in", to: "rentals#checkin", as: "checkin"
 end
