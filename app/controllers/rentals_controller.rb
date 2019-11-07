30ef43ebc9a2d40c941da456d7719812f62fb2d1
Janice Huang <janicehuang@gmail.com>
2019-11-07 14:12:11 -0800

    tweaks in rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -11,17 +11,17 @@ class RentalsController < ApplicationController
       return
     end
 
-    if movie.inventory < 1
+    if movie.available_inventory.nil? || movie.available_inventory < 1
       render json: {"errors" => ["not in stock"]}, status: :bad_request
       return
     end
     rental = Rental.new(rental_params)
     rental.checkout_date = Date.today
-    rental.due_date = checkout_date + 7
+    rental.due_date = rental.checkout_date + 7
 
     if rental.save
       render json: rental.as_json, status: :ok
-      movie.inventory -= 1
+      movie.available_inventory -= 1
       customer.movies_checked_out_count += 1
     else
       render json: { ok: false, errors: rental.errors.messages }, status: :bad_request
