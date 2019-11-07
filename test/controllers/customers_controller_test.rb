592af9f85b068fbaabeb2041f4be9683e89eb54f
daniela sanchez <dnsanche@gmail.com>
2019-11-06 17:16:45 -0800

    Create controller and tests

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,5 +1,6 @@
+CUSTOMER_KEYS_JSON = ["name", "registered_at", "address", "city", "state", "postal_code", "phone", "checked_out_count"]
+
 require "test_helper"
-CUSTOMER_KEYS = ["name", "registered_at", "address", "city", "state", "postal_code", "phone", "checked_out_count"]
 
 describe CustomersController do
   describe "index" do
@@ -20,7 +21,7 @@ describe CustomersController do
       expect(body).must_be_instance_of Array
       body.each do |customer|
         expect(customer).must_be_instance_of Hash
-        expect(customer.keys.sort).must_equal CUSTOMER_KEYS
+        expect(customer.keys).must_equal CUSTOMER_KEYS_JSON
       end
     end
 
@@ -37,4 +38,54 @@ describe CustomersController do
       expect(body).must_equal []
     end
   end  
+
+  describe "show" do 
+    it "responds with JSON and success" do
+      customer = customers(:one)
+      
+      get customer_path(customer)
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Hash
+      expect(body.keys).must_equal CUSTOMER_KEYS_JSON
+      expect(body["name"]).must_equal "Dani"
+    end
+
+    it "returns not found error if invalid customer" do
+      invalid_id = -1
+      get customer_path(invalid_id)
+
+      body = JSON.parse(response.body)
+
+      must_respond_with :not_found 
+      expect(body["errors"]).must_equal ["not found"]     
+    end
+  end
+
+  describe "create" do
+    let(:customer_info) {
+      {
+        customer: {
+          name: 'Tom'
+        }
+      }
+    }
+    it "can create a new customer" do
+      expect { post customers_path, params: customer_info }.must_differ 'Customer.count', 1
+
+      must_respond_with :created
+    end
+
+    it "will respond with bad_request for invalid data" do
+      customer_info[:customer][:name] = nil
+    
+      expect { post customers_path, params: customer_info }.wont_change "Customer.count"
+    
+      must_respond_with :bad_request
+    
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body["errors"].keys).must_include "name"
+    end
+  end
 end
