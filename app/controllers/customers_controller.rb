9694d1357e237691ebb937779c7615060c11e66c
Hallie <idhallie@gmail.com>
2019-11-07 13:25:12 -0800

    Passes movies checked out test.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  KEYS = [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at]
+  KEYS = [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at, :movies_checked_out_count]
   
   def index
     customers = Customer.all.as_json(only: KEYS)
