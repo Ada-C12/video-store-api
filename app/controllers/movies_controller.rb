a03f39ecfbb09b2ba477d181e856626a7c43b8bd
Monick <monick.keo@gmail.com>
2019-11-06 19:20:32 -0800

    Removed excess movie fixtures and updated movie model tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,22 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies.as_json(only: MOVIE_FIELDS), status: :ok
   end 
+
+  def create
+    new_movie = Movie.new(movie_params)
+
+    if new_movie.save
+      render json: new_movie.as_json(only: [:id]), status: :created 
+      return
+    else 
+      render json: {ok: false, errors: new_movie.errors.messages}, status: :bad_request
+      return
+    end 
+  end 
+
+  private
+
+    def movie_params
+      params.require(:movie).permit(:id, :title, :release_date, :overview)
+    end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -34,4 +34,31 @@ describe MoviesController do
       expect(body).must_equal []
     end
   end 
+
+  describe "create" do
+    # let(:movie_data) {
+    #   { movie: { title: "Everything cool", release_date: "2019-09-09", overview: "Best movie ever!" }}
+    # }
+
+    # it "responds with created status when request is good" do
+    #   expect{ post movies_path, params: movie_data }.must_differ "Movie.count", 1
+    #   must_respond_with :created
+    
+    #   body = JSON.parse(response.body)
+    #   expect(body.keys).must_equal ['id']
+    # end
+
+    # it "responds with bad_request when request has no name" do 
+    #   # make bad request
+    #   pet_data[:pet][:name] = nil 
+    #   # call 
+    #   # verify count doesn't change
+    #   expect{post pets_path, params: pet_data}.wont_change "Pet.count"
+    #   # verify bad_request status
+    #   must_respond_with :bad_request
+    #   # body contains errors which contain string 'name'
+    #   body = JSON.parse(response.body)
+    #   expect(body['errors'].keys).must_include 'name'
+    # end 
+  end
 end
