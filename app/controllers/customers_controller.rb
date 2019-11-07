457f366caf0acb4c95c3814a560cd879204e724e
Janice Huang <janicehuang@gmail.com>
2019-11-06 20:45:11 -0800

    minor cleaning up of spaces, zomg

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,11 +2,6 @@ CUSTOMER_KEYS = ["id", "name", "address", "city", "state", "postal_code", "phone
 
 class CustomersController < ApplicationController
 
-  # def zomg
-  #   working = "it works!"
-  #   render json: working, status: :ok
-  # end
-
   def index
   customers = Customer.all.as_json(only: CUSTOMER_KEYS)
   render json: customers, status: :ok
