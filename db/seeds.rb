35a4f59408b01ec5ce10e7fb1ede4350d7e38d69
eaball35 <eaball35@gmail.com>
2019-11-07 12:36:16 -0800

    updated seed for available_inventory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
   Customer.create!(customer)
+
 end
 
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
-  Movie.create!(movie)
+  new_movie = Movie.create!(movie)
+  new_movie.available_inventory = new_movie.inventory
+  new_movie.save
 end
