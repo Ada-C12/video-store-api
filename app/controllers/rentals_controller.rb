cebff51258e9d85309741f197439153599427f22
Brianna Kemp <kemp.bri@gmail.com>
2019-11-06 16:57:40 -0800

    Updated test naming error

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,24 +1,2 @@
 class RentalsController < ApplicationController
-  def checkout
-    new_rental = Rental.new(rental_params)
-    new_rental.checkout_date =  Time.now
-    new_rental.due_date =  Time.now + 7.days
-    if new_rental.save
-      render json: new_rental.as_json(only: [:id]), status: :ok
-      return
-    else
-      render json: { ok: false, errors: new_rental.errors.messages }, status: :bad_request
-      return
-    end
-  end
-  
-  def checkin
-    
-  end
-  
-  private
-  
-  def rental_params
-    params.permit(:movie_id, :customer_id, :checkout_date)
-  end
 end
