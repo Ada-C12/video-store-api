6f7bbf02623741050970c873252cc34ab4bcc71e
Dominique <dom_taylor@icloud.com>
2019-11-07 13:55:26 -0800

    latest commit

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -28,28 +28,28 @@ class RentalsController < ApplicationController
   end 
 
   def checkin
-    movie = Movie.find_by(id: rental_params[:movie_id])
-    customer = Customer.find_by(id: rental_params[:customer_id])
+    # movie = Movie.find_by(id: rental_params[:movie_id])
+    # customer = Customer.find_by(id: rental_params[:customer_id])
 
-    customer.movies_checked_out_count -= 1
-    movie.available_inventory += 1
+    # customer.movies_checked_out_count -= 1
+    # movie.available_inventory += 1
       
-      if customer.save && movie.save
-        render json: rental.as_json(only: [:id]), status: :ok
-        return
-      else
-        render json: {
-          ok: false,
-          errors: movie.errors.messages
-        }, status: :bad_request
-        return
-      end
-    else
-      render json: {
-        ok: false,
-        errors: "inventory unavailable"
-      }, status: :bad_request
-    end
+    #   if customer.save && movie.save
+    #     render json: rental.as_json(only: [:id]), status: :ok
+    #     return
+    #   else
+    #     render json: {
+    #       ok: false,
+    #       errors: movie.errors.messages
+    #     }, status: :bad_request
+    #     return
+    #   end
+    # else
+    #   render json: {
+    #     ok: false,
+    #     errors: "inventory unavailable"
+    #   }, status: :bad_request
+    # end
 
     
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get "/zomg", to: "controller/zomg", as: "zomg_path"
 
+  post "/rentals/check-in", to: "rentals#checkin", as: 'checkin'
+  post "/rentals/check-out", to: "rentals#checkout", as: 'checkout'
+  
   resources :customers, only: [:index]
   resources :movies, only: [:index, :show, :create]
-  
-  post "/rentals/checkin", to: "rentals#checkin", as: 'checkin'
-  post "/rentals/checkout", to: "rentals#checkout", as: 'checkout'
+
 end
