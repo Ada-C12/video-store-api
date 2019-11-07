43dc91dda4bf67c68e09afb738099ed637715fa7
daniela sanchez <dnsanche@gmail.com>
2019-11-06 17:38:11 -0800

    Customer model test for initialize and validations

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
-describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+describe Customer do 
+  describe "initialize" do
+    before do
+      @new_customer = Customer.new(name: "Janice")
+    end
+
+    it "can be instantiated" do
+      expect(@new_customer.valid?).must_equal true
+    end
+    
+    it "will have the required fields" do
+      CUSTOMER_KEYS.each do |field|
+        expect(@new_customer).must_respond_to field
+      end
+    end
+  end
+
+  describe "validations" do
+    describe "name" do
+      it "must have a name" do
+        customer_one = customers(:one)
+        customer_one.name = nil
+        
+        expect(customer_one.valid?).must_equal false
+        expect(customer_one.errors.messages).must_include :name
+        expect(customer_one.errors.messages[:name]).must_equal ["can't be blank"]
+      end
+    end
+  end
 end
