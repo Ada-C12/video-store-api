c4f3e1abd9389e88e7ea7f67f3ff18955d0ecba5
dora1405 <li.dora05@gmail.com>
2019-11-07 15:07:56 -0800

    3 tests passing for rentals controller. Creating a new rental and errors for invalid movie and customer

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,27 +2,31 @@ class RentalsController < ApplicationController
   def checkout
     movie = Movie.find_by(id: rental_params[:movie_id])
     if movie.nil?
+      puts "######################################"
       render json: {"errors" => ["not found"]}, status: :not_found
       return
     end
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     customer = Customer.find_by(id: rental_params[:customer_id])
     if customer.nil?
       render json: {"errors" => ["not found"]}, status: :not_found
       return
     end
+    puts "********************************************"
 
     if movie.available_inventory.nil? || movie.available_inventory < 1
       render json: {"errors" => ["not in stock"]}, status: :bad_request
       return
     end
+    p rental_params
     rental = Rental.new(rental_params)
     rental.checkout_date = Date.today
     rental.due_date = rental.checkout_date + 7
-
+    
     if rental.save
-      render json: rental.as_json, status: :ok
       movie.available_inventory -= 1
       customer.movies_checked_out_count += 1
+      render json: rental.as_json, status: :ok
     else
       render json: { ok: false, errors: rental.errors.messages }, status: :bad_request
       rental
@@ -35,6 +39,6 @@ class RentalsController < ApplicationController
   private
 
   def rental_params
-    params.require(:rental).permit(:checkout_date, :checkin_date, :customer_id, :movie_id, :due_date)
+    params.permit(:checkout_date, :checkin_date, :customer_id, :movie_id, :due_date)
   end
 end
