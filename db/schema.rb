cb71b437f78031d20f3d64a6b46302c6485bfe98
brilatimer <brihiramine@gmail.com>
2019-11-07 15:11:54 -0800

    migration added checkin, checkout, duedate for rentals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_215138) do
+ActiveRecord::Schema.define(version: 2019_11_07_230144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2019_11_05_215138) do
     t.integer "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "check_in"
+    t.datetime "check_out"
+    t.datetime "due_date"
   end
 
 end
