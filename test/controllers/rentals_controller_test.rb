f75807d4bc6a90c3da30d0638e419b7392974282
Kristina <kristina.tanya@gmail.com>
2019-11-07 13:56:25 -0800

    passing nominal and edge case for checkin rental

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -73,27 +73,26 @@ describe RentalsController do
     it "will return the checked in rental's id and status of ok" do
       movie = movies(:movie1)
       customer = customers(:customer1)
-      rental = Rental.create(movie: movie, customer: customer, checkout_date: Time.now, due_date: Time.now + 7)
-      
-      rental_data = {
-        rental: {
-          id: rental.id,
-        }
-      }
-      
-      post rental_checkin_path(rental.id), params: rental_data
+      rental = Rental.create!(movie: movie, customer: customer, checkout_date: Time.now, due_date: Time.now + 7)
+
+      rental_data = {id: rental.id}
+
+      post rental_checkin_path, params: rental_data
       body = JSON.parse(response.body)
       expect(body.keys).must_include "id"
       must_respond_with :ok
     end
 
+    it "will return a status of not_found if given invalid rental id" do
+      rental_data = {id: -1}
 
-    # it will find a rental by it's id
-    # it will increment the rental's movie's inventory by 1
-    # it will decrement the rental's customer's movies_checked_out_count by 1
-
-
-
+      post rental_checkin_path, params: rental_data
+      must_respond_with :not_found
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body["errors"]).must_include "not found"
+    end
   end
 
 end
