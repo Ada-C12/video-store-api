231510d2f3f20055ea1bf2652d1af835eef67a54
Janice Huang <janicehuang@gmail.com>
2019-11-07 15:46:07 -0800

    take out debugging stuff, write more passing tests

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,27 +2,25 @@ class RentalsController < ApplicationController
   def checkout
     movie = Movie.find_by(id: rental_params[:movie_id])
     if movie.nil?
-      puts "######################################"
       render json: {"errors" => ["not found"]}, status: :not_found
       return
     end
-    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
     customer = Customer.find_by(id: rental_params[:customer_id])
     if customer.nil?
       render json: {"errors" => ["not found"]}, status: :not_found
       return
     end
-    puts "********************************************"
 
     if movie.available_inventory.nil? || movie.available_inventory < 1
       render json: {"errors" => ["out of stock"]}, status: :bad_request
       return
     end
-    p rental_params
+
     rental = Rental.new(rental_params)
     rental.checkout_date = Date.today
     rental.due_date = rental.checkout_date + 7
-    
+
     if rental.save
       movie.available_inventory -= 1
       customer.movies_checked_out_count += 1
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -14,18 +14,24 @@ describe RentalsController do
         movie_id: movie.id
       }
     }
-    
+
     it "creates a new Rental with valid input" do
       expect{post checkout_path, params: rental_data}.must_change 'Rental.count', 1
-      
       must_respond_with :ok
-      
+    end
+
+    it "assigns the current date to checkout_date" do
+      post checkout_path, params: rental_data
+      rental = Rental.last
+      expect(rental.checkout_date).must_equal Date.today
+    end
+
+    it "assigns due date as a week from current date" do
+      post checkout_path, params: rental_data
+      rental = Rental.last
+      expect(rental.due_date).must_equal (Date.today + 7)
     end
     
-    it "assigns the current date to checkout_date" do
-    end
-    it "assigns due date as a week from current date" do
-    end
     it "throws an error if invalid movie" do
       rental_data[:movie_id] = nil
       expect {post checkout_path, params: rental_data}.wont_change "Rental.count"
@@ -39,19 +45,19 @@ describe RentalsController do
       expect(response.header['Content-Type']).must_include 'json'
     end
     it "throws an error if movie inventory is zero" do
-      
+
     end
     it "if rental successfully created, movie inventory is decreased by one" do
     end
     it "if rental succesfully created, custoer's movies checked out count increased by one" do
     end
   end
-  
+
   describe "checkin" do
     it "can successfully check a rental back in with valid input" do
     end
     it "increases movie's inventory by one if successfully checked back in" do
     end
-    
+
   end
 end
