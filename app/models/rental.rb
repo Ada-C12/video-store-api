7a2b774867043c734a4b3046cccf2aa11bd7a327
Rinostar <codingrinostar@gmail.com>
2019-11-07 13:32:30 -0800

    add validation & tests for rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,6 @@
 class Rental < ApplicationRecord
+  validates :checkout_date, presence: true
+  validates :due_date, presence: true
+  validates :movie_id, presence: true
+  validates :customer_id, presence: true
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    before do 
+      @rental = Rental.new(
+        checkout_date: Date.today,
+        due_date: Date.today + 7,
+        movie_id: movies(:movie2).id,
+        customer_id: customers(:customer3).id
+      )
+    end
+
+    it "is valid when all fields are present" do
+      result = @rental.valid?
+
+      expect(result).must_equal true
+    end
+
+    it "is invalid without a checkout_date" do
+      @rental.checkout_date = nil
+      result = @rental.valid?
+
+      expect(result).must_equal false
+    end
+
+    it "is invalid without a due_date" do
+      @rental.due_date = nil
+      result = @rental.valid?
+
+      expect(result).must_equal false
+    end
+
+    it "is invalid without a movie" do
+      @rental.movie_id = nil
+      result = @rental.valid?
+
+      expect(result).must_equal false
+    end
+
+    it "is invalid without a customer" do
+      @rental.customer_id = nil
+      result = @rental.valid?
+
+      expect(result).must_equal false
+    end
+  end
 end
