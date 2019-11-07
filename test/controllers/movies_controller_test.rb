b29f35953e47c96c81306545d5172063b45649ed
ohcloud <cloudylopez@icloud.com>
2019-11-06 16:52:18 -0800

    started the show tests for the movies controller.

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,6 +1,19 @@
 require "test_helper"
 
 describe MoviesController do
+
+  MOVIE_FIELDS = ["id", "release_date", "title"]
+  
+  def check_response(expected_type:, expected_status: :success)
+    must_respond_with expected_status
+    expect(response.header['Content-Type']).must_include 'json'
+    
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    
+    return body
+  end
+  
   
   describe "index" do 
     
@@ -13,7 +26,6 @@ describe MoviesController do
       must_respond_with :ok
     end
     
-    MOVIE_FIELDS = ["id", "release_date", "title"]
     
     it "responds with an array of movie hashes" do 
       # Act
@@ -38,6 +50,22 @@ describe MoviesController do
       expect(body).must_equal []
       
     end
+  end
+  
+  describe "show" do
+    
+    it "will retrieve one single movie" do
+      #arrange 
+      good_movie = Movie.first
+      
+      #act
+      get movie_path(good_movie)
+      body = check_response(expected_type: Hash)
+      
+      #assert
+      expect(body.keys.sort).must_equal MOVIE_FIELDS
+    end
+    
   end
   
 end#end of moviescontroller
