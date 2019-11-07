88350d68a2f26cd9af7ecf4a64b0309735f8ddd7
Ga-Young Jin <gyjinn@gmail.com>
2019-11-07 10:27:47 -0800

    added movies checked out count to customer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_220614) do
+ActiveRecord::Schema.define(version: 2019_11_07_181828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_220614) do
     t.string "postal_code"
     t.string "phone"
     t.datetime "registered_at"
+    t.integer "movies_checked_out_count", default: 0
   end
 
   create_table "movies", force: :cascade do |t|
