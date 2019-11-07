b5da031792ecec0ee5deddde69350b09aa6ff1fa
eaball35 <eaball35@gmail.com>
2019-11-06 18:03:51 -0800

    testing on checkout

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -9,12 +9,16 @@ describe RentalsController do
         }
     }
     
-    it "responds with JSON and success when passed in valid params" do
-      post checkout_path, params: rental_data
+    it "responds with JSON and success when passed in valid params and creates new rental" do
+      expect {
+        post checkout_path, params: rental_data
+      }.must_differ 'Rental.count', 1
 
+      body = JSON.parse(response.body)
+      
+      expect(body["id"]).must_be_instance_of Integer
       expect(response.header['Content-Type']).must_include 'json'
       must_respond_with :ok
     end
-  
   end
 end
