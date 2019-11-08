f46d23be8477a6bc8490bb17483f2bfa3b57f369
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-11-08 10:58:36 -0800

    Wrote customer controller tests.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
-  has_many :rentals
+  has_many :rentals, dependent: :nullify
 validates :name, presence: true
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "responds with JSON and success" do
+      get customers_path
+
+      expect(response.header["Content-Type"]).must_include "json"
+      must_respond_with :ok
+    end
+    it "responds with the expected list of movies" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Array
+      expect(body.size).must_equal Customer.count
+      body.each do |customer_hash|
+        expect(customer_hash).must_be_instance_of Hash 
+      end
+    end
+    it "will respond with an empty array when there are no movies" do
+      Customer.destroy_all
+
+      get customers_path
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
+  end
 end
