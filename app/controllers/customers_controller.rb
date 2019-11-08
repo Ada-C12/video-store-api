af8c57f08c39b4002c5b023bcb12ad86b765a938
gracexinran <gracexinran@gmail.com>
2019-11-07 22:34:15 -0800

    Implemented sort and paginate functions to customers controller index action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,15 +1,15 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all
+    
     if params[:sort]
       customers = Customer.order(params[:sort])
-    elsif params[:n] && params[:p] && !params[:sort]
-      customers = Customer.page(params[:p])
-    elsif params[:n] && params[:p] && params[:sort]
-      customers = Customer.paginate(page: params[:p], per_page: params[:n])
-    else
-      customers = Customer.all
     end
-
+    
+    if params[:p] || params[:n]
+      customers = customers.paginate(page: params[:p], per_page: params[:n])
+    end
+    
     find_customers = customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count])
     render json: find_customers, status: :ok
   end
