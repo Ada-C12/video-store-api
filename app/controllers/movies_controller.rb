2218c2cc4226fa006ff93594488d9c02161c5825
m.dove@live.com <m.dove@live.com>
2019-11-08 13:31:31 -0800

    added decrementing and incrementing of available_inventory. Smoke tests passing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,9 +8,13 @@ class MoviesController < ApplicationController
   def show
     movie = Movie.find_by(id: params[:id])
     if movie
+      if !movie.available_inventory && movie.available_inventory != 0
+        movie.available_inventory = movie.inventory
+        movie.save
+      end
       render json: movie.as_json(only: [:title, :overview, :release_date, :inventory, :available_inventory])
       return
-    else 
+    else
       render json: { errors: ["Bad Request"] }, status: :bad_request
       return
     end
@@ -18,19 +22,20 @@ class MoviesController < ApplicationController
 
   def create
     new_movie = Movie.new(movie_params)
+    new_movie.available_inventory = movie_params[:inventory]
 
     if new_movie.save
       render json: new_movie.as_json(only: [:id]), status: :ok
       return
     else
-      render json: { errors: ["BAD Request"]}, status: :bad_request
+      render json: { errors: ["BAD Request"] }, status: :bad_request
       return
     end
   end
 end
 
-
 private
+
 def movie_params
   params.permit(:title, :overview, :release_date, :inventory, :available_inventory)
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,17 +1,17 @@
 require "test_helper"
 
 describe RentalsController do
-  describe "check_out" do 
-    it "creates a new instance of Rental if the movie is available" do 
+  describe "check_out" do
+    it "creates a new instance of Rental if the movie is available" do
       movie = movies(:movie1)
       customer = customers(:customer1)
 
       new_rental = {
-        customer_id: customer.id, 
-        movie_id: movie.id
+        customer_id: customer.id,
+        movie_id: movie.id,
       }
-      expect{
-      post check_out_path(new_rental)
+      expect {
+        post check_out_path(new_rental)
       }.must_differ "Rental.count", 1
 
       expect(new_rental[:customer_id]).must_equal customer.id
@@ -19,13 +19,13 @@ describe RentalsController do
       body = JSON.parse(response.body)
       expect(body).must_be_instance_of Hash
     end
-    
-    # it "returns an error and doesn't make in instance of Rental if movie is not available" do 
+
+    # it "returns an error and doesn't make in instance of Rental if movie is not available" do
     #   movie = movies(:movie3)
     #   customer = customers(:customer1)
 
     #   new_rental = {
-    #     customer_id: customer.id, 
+    #     customer_id: customer.id,
     #     movie_id: movie.id
     #   }
     #   expect{
@@ -37,19 +37,24 @@ describe RentalsController do
     #   expect(response.header["Content-Type"]).must_include "json"
     #   expect(body).must_be_instance_of Hash
     #   expect(body.keys).must_include "errors"
-    # end 
+    # end
   end
 
-  describe "check_in" do 
-    it "changes the status of the given rental to checked in" do 
-    customer = customers(:customer1)
-    movie = movies(:movie1)
+  describe "check_in" do
+    it "changes the status of the given rental to checked in" do
+      customer = customers(:customer1)
+      movie = movies(:movie1)
 
-    test_rental = rentals(:rental1)
+      rental = {
+        customer_id: customer.id,
+        movie_id: movie.id,
+      }
 
-    post check_in_path(test_rental)
+      post check_in_path(rental)
 
-    expect(test_rental.status).must_equal "checked in"
+      test_rental = rentals(:rental1)
+
+      expect(test_rental.status).must_equal "checked_in"
     end
   end
 end
