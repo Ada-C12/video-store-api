89bea1ca619978d4368a037c8a441deaff551d26
brilatimer <brihiramine@gmail.com>
2019-11-08 13:28:58 -0800

    checkin started

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,7 +2,7 @@ class RentalsController < ApplicationController
   def checkout 
     movie = Movie.find_by(id: rental_params[:movie_id])
     customer = Customer.find_by(id: rental_params[:customer_id])
-
+    
     if movie.available_inventory != 0 
       new_rental = Rental.new(rental_params)
       new_rental.check_out = Time.now
@@ -21,11 +21,26 @@ class RentalsController < ApplicationController
       render json: {ok: false, errors: new_rental.errors.messages}, status: :bad_request
       return
     end 
-
-  end 
-
-  private
-  def rental_params
-    params.permit(:customer_id, :movie_id, :checkout, :check_in, :check_out, :due_date)
+    
+    def checkin 
+      rental = Rental.find_by(id: rental_params[:customer_id])
+      
+      new_rental.check_in = Time.now
+      new_rental.movie.available_inventory += 1
+      
+      if new_rental.save && new_rental.movie.save
+        render json: new_rental.as_json(only: [:check_in]), status: :ok 
+        return
+      else 
+        render json: {ok: false, errors: new_rental.errors.messages}, status: :bad_request
+        return
+      end 
+      
+    end 
+    
+    private
+    def rental_params
+      params.permit(:customer_id, :movie_id, :checkout, :check_in, :check_out, :due_date)
+    end
   end
-end
+end 
