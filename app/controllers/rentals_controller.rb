14594676c15b84b1fc0a8ed7190f51707054ab15
Monick <monick.keo@gmail.com>
2019-11-07 22:03:48 -0800

    added tests for checking json and checkout and due date. Added one for inventory but it's not working

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -8,7 +8,7 @@ class RentalsController < ApplicationController
     if new_rental.save
       new_rental.check_out = Time.now
       new_rental.due_date = new_rental.check_out + (7*24*60*60)
-
+      
       new_rental.movie.available_inventory -= 1 
       
       render json: new_rental.as_json(only: [:customer_id, :movie_id]), status: :created 
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -13,17 +13,23 @@ describe RentalsController do
     
       expect{ post checkout_path, params: rental_hash }.must_differ "Rental.count", 1
 
-      # rental = Rental.find_by(id: Rental.id)
-      # expect(rental.check_out).must_equal 2019-11-07
-      # expect(rental.due_date).must_equal 2019-11-14
+      body = JSON.parse(response.body)
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :created
+
+      rental = Rental.find_by(movie_id: movie.id)
+      
+      expect(rental.check_out).must_be_kind_of Time
+      expect(rental.due_date).must_be_kind_of Time
+      binding.pry
+      expect(rental.movie.available_inventory).must_equal 9
     end 
 
-    it "assigns the checkout date" do 
-    end 
-    
-    it "assigns due date one week from checkout date" do 
+    it "sends response for bad request" do 
     end 
 
+    it "should not allow rental if available inventory is 0" do 
+    end 
 
   end 
 end
