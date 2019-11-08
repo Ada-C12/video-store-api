48c0786d61296dbe6c535c5b479a5be0932f9b16
Monick <monick.keo@gmail.com>
2019-11-07 22:10:02 -0800

    added test for bad request

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 describe RentalsController do
   describe "checkout" do 
-    it "checks out movie to customer by creating a Rental" do 
-      movie = movies(:m1)
-      customer = customers(:c1)
+    let(:movie){movies(:m1)}
+    let(:customer){customers(:c1)}
 
+    it "checks out movie to customer by creating a Rental" do
       rental_hash = {
           customer_id: customer.id, 
           movie_id: movie.id
@@ -21,14 +21,25 @@ describe RentalsController do
       
       expect(rental.check_out).must_be_kind_of Time
       expect(rental.due_date).must_be_kind_of Time
-      binding.pry
+      # binding.pry
       expect(rental.movie.available_inventory).must_equal 9
     end 
 
     it "sends response for bad request" do 
+      rental_hash = {
+        customer_id: nil, 
+        movie_id: movie.id
+      }
+        
+      expect{ post checkout_path, params: rental_hash }.wont_change "Rental.count"
+      must_respond_with :bad_request
+        
+      body = JSON.parse(response.body)
+      expect(body['errors'].keys).must_include 'customer_id'
     end 
 
     it "should not allow rental if available inventory is 0" do 
+      #INVENTORY DECREMENT IS NOT WORKING RIGHT NOW, COME BACK TO THIS
     end 
 
   end 
