96b65ddf4b1ec5341767366de45de801ef518ee7
Nicky Choi <nickyjinchoi@gmail.com>
2019-11-07 17:10:14 -0800

    Added pagination gem and refactored Customers Controller index action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,16 @@
 class CustomersController < ApplicationController
   def index
-    customers = Customer.all.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count])
-    render json: customers, status: :ok
+    if params[:sort]
+      customers = Customer.order(params[:sort])
+    elsif params[:n] && params[:p] && !params[:sort]
+      customers = Customer.page(params[:p])
+    elsif params[:n] && params[:p] && params[:sort]
+      customers = Customer.paginate(page: params[:p], per_page: params[:n])
+    else
+      customers = Customer.all
+    end
+
+    find_customers = customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count])
+    render json: find_customers, status: :ok
   end
 end
