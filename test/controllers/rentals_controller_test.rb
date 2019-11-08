116ef4fa767b0fe1e5ed31afad03caef7154ae67
brilatimer <brihiramine@gmail.com>
2019-11-08 14:29:01 -0800

    test written for if rental is not found

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -77,36 +77,21 @@ describe RentalsController do
       movie.reload 
       expect(movie.available_inventory).must_equal 10
     end 
+
+    it "returns not_found if rental is not found" do 
+      updates = { rental: { checkin: Time.now } }
+      expect {patch checkin_path(-100), params: updates}.wont_change "Rental.count"  
+      must_respond_with :not_found
+    end
     
-    #   it "sends response for bad request" do 
-    #     rental_hash = {
-    #       customer_id: nil, 
-    #       movie_id: movie.id
-    #     }
+      # it "sends response for bad request" do 
     
-    #     expect{ post checkout_path, params: rental_hash }.wont_change "Rental.count"
-    #     must_respond_with :bad_request
+      #   expect{ post checkout_path, params: rental_hash }.wont_change "Rental.count"
+      #   must_respond_with :bad_request
     
-    #     body = JSON.parse(response.body)
-    #     expect(body['errors'].keys).must_include 'customer_id'
-    #   end 
-    
-    #   it "should not allow rental if available inventory is 0" do 
-    #     rental_hash = {
-    #       customer_id: customer.id, 
-    #       movie_id: movies(:m2).id
-    #     }
-    
-    #     expect{ post checkout_path, params: rental_hash }.wont_change "Rental.count"
-    #     must_respond_with :bad_request
-    
-    #     body = JSON.parse(response.body)
-    #     expect(body['errors']).must_equal "available inventory is 0"
-    #   end 
-    
-    # end 
-    
-    
-    
+      #   body = JSON.parse(response.body)
+      #   expect(body['errors'].keys).must_include 'customer_id'
+      # end     
+   
   end
 end
