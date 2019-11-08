79e24054a1681bc3b744fec59fe3b5a762401358
dora1405 <li.dora05@gmail.com>
2019-11-07 20:54:45 -0800

    codes for checkin action and tests. 3 tests errors and need to find existing rental id

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -5,22 +5,22 @@ class RentalsController < ApplicationController
       render json: {"errors" => ["not found"]}, status: :not_found
       return
     end
-
+    
     customer = Customer.find_by(id: rental_params[:customer_id])
     if customer.nil?
       render json: {"errors" => ["not found"]}, status: :not_found
       return
     end
-
+    
     if movie.available_inventory.nil? || movie.available_inventory < 1
       render json: {"errors" => ["out of stock"]}, status: :bad_request
       return
     end
-
+    
     rental = Rental.new(rental_params)
     rental.checkout_date = Date.today
     rental.due_date = rental.checkout_date + 7
-
+    
     if rental.save
       movie.available_inventory -= 1
       movie.save
@@ -32,12 +32,34 @@ class RentalsController < ApplicationController
       rental
     end
   end
+  
   def checkin
-    # add back to inventory
+    # find rental from rental id
+    puts "*************************************"
+    rental = Rental.find_by(id: rental_params[:id])
+    p rental
+    # update checkin_date
+    rental.checkin_date = Date.today
+    puts "#####################################"
+    movie = Movie.find_by(id: rental_params[:movie_id])
+    customer = Customer.find_by(id: rental_params[:customer_id])
+    
+    # update rental
+    if rental.update
+      movie.available_inventory += 1
+      movie.save
+      customer.movies_checked_out_count -= 1
+      customer.save
+      render json: rental.as_json, status: :ok
+    else
+      render json: { ok: false, errors: rental.errors.messages }, status: :bad_request
+      rental
+    end
+    
   end
-
+  
   private
-
+  
   def rental_params
     params.permit(:checkout_date, :checkin_date, :customer_id, :movie_id, :due_date)
   end
